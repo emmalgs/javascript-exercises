c41820ba28eebdde85b2d4243e6c230f14ffd757
Emma Gerigscott <elgerig@gmail.com>
2022-04-15 13:44:37 -0700

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    const array = text.split("")
+    array.reverse();
+    return array.join('');
 };
 
 // Do not edit below this line
