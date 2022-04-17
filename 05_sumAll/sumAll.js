97cac9d54ea9d27f170200010c7b9984598c82b3
Emma Gerigscott <elgerig@gmail.com>
2022-04-17 15:46:38 -0700

    complete exercise 5 TDD

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+    let sum = 0;
+    if (firstNum < 0 || lastNum < 0) return `ERROR`;
+    if (!Number.isInteger(firstNum) || !Number.isInteger(lastNum)) return 'ERROR';
+     else if (lastNum < firstNum) {
+        for (i=lastNum; i<firstNum + 1; i++) {
+        sum += i;
+    }} else {
+    for (i=firstNum; i<lastNum + 1; i++) {
+        sum += i;
+        }
+    }   
+    return sum
 };
 
 // Do not edit below this line
