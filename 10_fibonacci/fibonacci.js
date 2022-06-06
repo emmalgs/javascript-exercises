fa9be5e2917423355764d7217c92fb57334c6e2b
Emma Gerigscott <elgerig@gmail.com>
2022-06-06 14:15:35 -0700

    complete fibonacci test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    if (num < 0) return "OOPS";
+    let a = 0,
+    b = 1,
+    temp;
 
+    while (num >= 0) {
+        temp = a;
+        a += b;
+        b = temp;
+        num--;
+    }
+    return b;
 };
 
 // Do not edit below this line
