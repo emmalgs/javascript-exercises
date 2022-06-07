1568c3b4f018bad9ae0ff9052e1c2741ef562d6e
Emma Gerigscott <elgerig@gmail.com>
2022-06-07 10:58:56 -0700

    just moving on

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,15 +1,5 @@
-const books = [
-    {
-      title: 'Book',
-      author: 'Name'
-    },
-    {
-      title: 'Book2',
-      author: 'Name2'
-    }
-  ]
 
-const getTheTitles = myFunction = (books) => books.map(function(book) {
+const getTheTitles = (books) => books.map(function(book) {
     return `${book.title}`
 });
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const isLiving = array.some((person) => {
+        if (typeof person.yearOfDeath === `undefined`) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    });
+    const oldest =
+    array.sort((a, b) =>
+    a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? 1 : -1
+);
+    return oldest[oldest.length -1];
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
